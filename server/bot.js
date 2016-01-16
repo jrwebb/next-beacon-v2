f9c125da4b0b951071375dc138653aab5a46924b
Arjun Gadhia <arjungadhia@gmail.com>
2016-01-16 12:54:47 +0000

    better language processing

--- server/bot.js
+++ server/bot.js
@@ -3,39 +3,33 @@
 const KeenQuery = require('n-keen-query');
 const Botkit = require('botkit');
 const controller = Botkit.slackbot();
-const didyoumean = require('didyoumean');
+const processBotCommand = require('./lib/process-bot-command');
 
-let allQueries = [];
-let questions = [];
-
-//Get all queries and store the questions in an array for later lookup
-KeenQuery.aliases.poll().then(() => {
-	allQueries = KeenQuery.aliases.get('');
-	questions = allQueries.map(q => q.question).filter(q => !!q);
-});
 
 const bot = controller.spawn({
   token: process.env.SLACK_KEENBOT_TOKEN
 });
 
-const generateResponse = (results) => ({
+const generateResponse = (results) => {
+	return {
 		attachments: [
 		{
       fallback: "Complete",
-      title: 'Results for ' + results.name,
+      title: results.question,
       color: "#7CD197",
       fields: results.headings.map((heading, index) => {
       	return {
       		title: heading,
-      		value: results.rows[index].reduce((prev, curr) => prev + curr + '|', ''),
+      		value: results.rows[index].join(' | '),
       		short: false
       	};
     	})
   	}]
-});
+	}
+};
 
 
-bot.startRTM(function(err,bot) {
+bot.startRTM(function(err) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
@@ -45,31 +39,42 @@ const convertToQuery = (message) => message.replace(/\&gt;/g, '>');
 
 controller.hears(['run (.*)'],["direct_message","direct_mention","mention"], (bot, message) => {
 	const query = convertToQuery(message.match[1]);
-	KeenQuery.build(query).print()
+	KeenQuery.execute(query)
 	.then(results => {
 			bot.reply(message, generateResponse(results))
 	});
 });
 
 
+
+const askQuestion = (query, bot, message) => {
+	if(!query.query) {
+		bot.reply(message, 'There appears to be no query attached to that question...check the spreadsheet');
+		return Promise.resolve();
+	}
+	return KeenQuery.buildFromAlias(query).print().then((res) => {
+		res.question = query.question || query.label || query.name;
+		return res;
+	});
+}
+
+
 controller.hears(['(.*)'],["direct_message","direct_mention","mention"], (bot, message) => {
 	const q = message.match[0].trim();
-	const question = didyoumean(q, questions) || q;
+	const queries = processBotCommand(q);
 
-	const queries = allQueries.filter((query) => {
-		return query.question && (query.question === question || query.name.indexOf(question) === 0);
-	});
 	if(!queries || queries.length === 0) {
-		bot.reply(message, 'sorry, dont understand you');
-	} else {
-		bot.reply(message, `I will now run query for question ${question}`);
+		bot.reply(message, 'Sorry, I don\'t understand your question. You can add queries in the spreadsheet here: https://docs.google.com/spreadsheets/d/1jH15yE5T6omD-B58UJfu1y4j8qN4QIs17u-52_Jkw7M/edit#gid=0');
 	}
-	Promise.all(queries.filter(query => !!query.query).map(query => KeenQuery.build(query.query.split('->print')[0]).print()))
+	Promise.all(queries.map(query => askQuestion(query, bot, message)))
 	.then(res => {
 		res.forEach(results => {
 			bot.reply(message, generateResponse(results))
 		});
 	})
-	.catch(console.error);
+	.catch((err) => {
+		bot.reply(message, "Sorry, I couldn't answer your question :(: ```" + err + "```");
+		console.error(err);
+	});
 
 });
