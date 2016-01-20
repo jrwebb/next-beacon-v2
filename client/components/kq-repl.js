15648bcdc209e76eaf9b29570060ab5cd19b7f70
Rhys Evans <rhys.evans@ft.com>
2016-01-20 14:53:49 +0000

    back to beacon link fro explorer

--- client/components/kq-repl.js
+++ client/components/kq-repl.js
@@ -1,9 +1,64 @@
 'use strict';
 
 const KeenQuery = require('n-keen-query');
+const debounce = function(fn,delay){
+		let timeoutId;
+		return function debounced(){
+				if(timeoutId){
+						clearTimeout(timeoutId);
+				}
+				const args = arguments;
+				timeoutId = setTimeout(function() {
+						fn.apply(this, args);
+				}.bind(this), delay);
+		};
+};
+
+class PropertySearch {
+
+	constructor () {
+		this.searchEl = document.querySelector('.kq-repl__properties-filter');
+		this.items = [].slice.call(document.querySelectorAll('.kq-repl__reference--properties li'))
+			.map(el => {
+				return {
+					el,
+					property: el.getAttribute('data-property')
+				}
+			});
+	}
+	init () {
+		const self = this;
+
+		this.onType = debounce(this.onType, 150).bind(this);
+
+		this.searchEl.addEventListener('keyup', function(ev) {
+			switch(ev.which) {
+				case 13 : return; // enter
+				case 9 : return; // tab
+				case 40 : return;
+				default :
+					self.onType(ev);
+				break;
+			}
+		});
+	}
+
+	onType () {
+		const str = this.searchEl.value.toLowerCase();
+		this.items.forEach(item => {
+			if (item.property.indexOf(str) === -1) {
+				item.el.classList.add('hidden');
+			} else {
+				item.el.classList.remove('hidden');
+			}
+		})
+	}
+}
 
 module.exports = {
 	init: () => {
+
+		new PropertySearch().init();
 
 		const input = document.querySelector('.kq-repl__input');
 		const output = document.querySelector('.kq-repl__output');
@@ -19,13 +74,13 @@ module.exports = {
 		}
 
 		input.addEventListener('keydown', ev => {
-			if (ev.keyCode == 13) {
+			if (ev.keyCode === 13) {
 				ev.preventDefault();
 			}
 		})
 
 		input.addEventListener('keyup', ev => {
-			if (ev.keyCode == 13) {
+			if (ev.keyCode === 13) {
 				run();
 				return false;
 			}
--- server/jobs/keen-properties.js
+++ server/jobs/keen-properties.js
@@ -6,7 +6,15 @@ module.exports = new Poller({
 	url: `https://api.keen.io/3.0/projects/${process.env.KEEN_PROJECT_ID}/events/cta:click?api_key=${process.env.KEEN_READ_KEY}`,
 	defaultData: [],
 	autostart: true,
-	parseData: data => Object.keys(data.properties).filter(name => name.indexOf('.querystring') === -1)
+	parseData: data => Object.keys(data.properties)
+	.filter(name => name.indexOf('.querystring') === -1)
+	.sort()
+	.map(name => {
+		return {
+			name,
+			lowerCase: name.toLowerCase()
+		}
+	})
 })
 
 
