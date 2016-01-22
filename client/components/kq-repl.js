72f5488652788fb8a98bc63bc86358112e310ffb
Rhys Evans <wheresrhys@gmail.com>
2016-01-22 10:42:33 +0000

    bugfix

--- client/components/kq-repl.js
+++ client/components/kq-repl.js
@@ -116,7 +116,7 @@ module.exports = {
 		del.on('click', '.kq-repl__reference--extractions li', ev => {
 			validate(input.value + '->' + ev.target.getAttribute('data-str'))
 				.then(str => {
-					input.value += str;
+					input.value = str;
 					input.focus();
 				}, e => {
 					output.innerHTML = `<span class="error">${e.message || e}</span>`;
