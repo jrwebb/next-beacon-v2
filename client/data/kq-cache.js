fb934c54490d6f05f8b8c50ce3751b6d203aef50
Rhys Evans <rhys.evans@ft.com>
2016-02-08 18:53:40 +0000

    button to copy chart data

--- client/data/kq-cache.js
+++ client/data/kq-cache.js
@@ -2,7 +2,7 @@
 const cache = {};
 
 export function retrieveKq (name) {
-	return cache[name]
+	return cache[name];
 }
 
 export function storeKq (name, kq) {
--- client/printers/index.js
+++ client/printers/index.js
@@ -6,3 +6,14 @@ import {printer as bigNumber} from './big-number';
 supportedChartTypes.forEach(chartType => KeenQuery.definePrinter(chartType, googleChartPrinterFactory(chartType)))
 
 KeenQuery.definePrinter('big-number', bigNumber);
+
+// Used to generate Tab separated values for copy paste
+// Can't use definePrinter here as the printer API is promisey
+// which conflicts with browseres' strict conditions for when
+// the document.execCommand('copy') API is allowed.
+KeenQuery.prototype.toTSV = function () {
+	const table = this.getTable().humanize('human');
+	return [table.headings].concat(table.rows)
+		.map(row => row.join('\t'))
+		.join('\n');
+}
