31587c6396a32ecc440abef663cfd15fbbdde834
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2016-02-15 12:15:52 +0000

    Removed redundant 'update' feature

--- server/middleware/aliases.js
+++ server/middleware/aliases.js
@@ -2,10 +2,6 @@
 
 let aliases = [];
 const Poller = require('ft-poller');
-const fs = require('fs');
-const denodeify = require('denodeify');
-const readFile = denodeify(fs.readFile);
-const writeFile = denodeify(fs.writeFile);
 const url = 'http://bertha.ig.ft.com/view/publish/gss/1jH15yE5T6omD-B58UJfu1y4j8qN4QIs17u-52_Jkw7M/aliases';
 
 const poller = new Poller({
@@ -17,25 +13,6 @@ const poller = new Poller({
 	}
 });
 
-function update () {
-
-	// Always update from Bertha
-	const update = fetch(url)
-		.then(res => res.json())
-		.then(json => {
-			writeFile(`${process.env.HOME}/.kq-aliases`, JSON.stringify(json, null, '\t'))
-			return json
-		});
-
-	// If local cache exists use it, otherwise wait for bertha
-	return readFile(`${process.env.HOME}/.kq-aliases`, 'utf8')
-		.then(file => {
-			return JSON.parse(file);
-		}, () => {
-			return update;
-		})
-}
-
 function get (name) {
 	if (name) {
 		return aliases.filter(a => a.name && a.name.indexOf(name) === 0);
@@ -46,13 +23,10 @@ function get (name) {
 
 module.exports = {
 	init: (req, res, next) => {
-		update();
-
 		res.locals.aliases = get().reduce((alias,item) => {
 			alias[item.name] = item;
 			return alias;
 		}, {});
-
 		next();
 	},
 
