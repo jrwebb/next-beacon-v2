4620c511d2f003f2d86d37b14112bef112cd9e6e
Rhys Evans <wheresrhys@gmail.com>
2016-03-09 12:01:49 +0000

    Silence the service worker

--- client/worker.js
+++ client/worker.js
@@ -11,11 +11,11 @@ function putInCache(request, res) {
 }
 
 self.addEventListener('install', function () {
-	console.log('install')
+	// console.log('install')
 });
 
 self.addEventListener('activate', function() {
-	console.log('activate')
+	// console.log('activate')
 	if (self.clients && clients.claim) {
 		clients.claim();
 	}
@@ -23,23 +23,22 @@ self.addEventListener('activate', function() {
 
 self.addEventListener('fetch', function (event) {
 	var request = event.request;
-	console.log('real fetch', request.url)
+	// console.log('real fetch', request.url)
 	if (/www\.gstatic\.com\//.test(request.url)) {
 		console.log('trying to find in cache', request.url)
 		return event.respondWith(
-      caches.match(request)
-        .then(function (response) {
-
-        	if (response) {
-        		console.log('delivering cached', request.url);
-        		return response.clone();
-        	}
-					console.log('failed to find in cache', request.url)
-					return fetch(request)
-						.then(res => {
-							putInCache(request, res);
-							return res.clone();
-						})
+		      caches.match(request)
+		        .then(function (response) {
+				if (response) {
+					// console.log('delivering cached', request.url);
+					return response.clone();
+				}
+					// console.log('failed to find in cache', request.url)
+				return fetch(request)
+					.then(res => {
+						putInCache(request, res);
+						return res.clone();
+					})
         })
     );
 	}
