8d0efce234315b5ef36fd494927dd329765f4612
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2016-03-16 15:14:06 +0000

    Currently working — by changing window location

--- client/components/global-configurator.js
+++ client/components/global-configurator.js
@@ -3,13 +3,19 @@
 const el = document.querySelector('.global__configurator__form');
 el.addEventListener('change', (e) => {
 	if (e.preventDefault) e.preventDefault();
-
 	const form = e.currentTarget;
 	const timeframeValue = form.querySelector('.timeframe').value;
+	let querystring = '?' + timeframeValue;
 
-	let querystring = '?' + encodeURIComponent(timeframeValue);
-	history.pushState({}, "", querystring);
+	// Todo: Tell the charts to update and reprint themselves (based on querystring perhaps?)
+	// [].forEach.call(document.querySelectorAll('.chart .chart__configurator [name]'), chartEl => {
+	// 	//del.on('change', '.chart__configurator [name]', reprint);
+	// 	console.log(chartEl);
+	// 	chartEl.dispatchEvent(new Event('change'));
+	// });
 
-//	window.location = `/dashboard/${document.querySelector('.featureFlagText').value}`;
+	// history.pushState({}, "", querystring);
+	window.location = querystring;
+
 	return false;
 });
