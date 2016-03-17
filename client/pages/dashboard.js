d0663bc9030d0c187efa9b61921bdf17130ba752
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2016-03-17 13:32:06 +0000

    Just a refactor

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -3,48 +3,52 @@
 import KeenQuery from 'keen-query';
 import {init as chartUi} from '../components/chart-ui';
 import {renderChart, displayError} from '../components/chart';
-import {fromQueryString as getConfigurator} from '../components/configurator';
+import {fromQueryString as getConfiguratorFromQueryString} from '../components/configurator';
 import {storeKq} from '../data/kq-cache';
 
+// export function renderAllCharts () {}
+
+function buildAndRenderChart (chart, printerEl) {
+	try {
+		let builtQuery = KeenQuery.buildFromAlias(chart);
+
+		// todo: default to column chart if it has dimension but no interval... or something
+		builtQuery = builtQuery.setPrinter(chart.printer || 'LineChart').tidy();
+
+		if (chart.hasConfigurableInterval) {
+			// avoid showing as big number when the default view could easily be converted to a line graph over time
+			if (builtQuery.dimension < 2 && (['AreaChart','LineChart','ColumnChart'].indexOf(chart.printer) > -1 || !chart.printer)) {
+				builtQuery = builtQuery.interval('d')
+			}
+		}
+
+		storeKq(chart.name, builtQuery);
+
+		const configuratorSkipSteps = [];
+
+		if (!chart.hasConfigurableInterval) {
+			configuratorSkipSteps.push('interval');
+		}
+
+		if (!chart.hasConfigurableTimeframe) {
+			configuratorSkipSteps.push('timeframe');
+		}
+
+		const configure = getConfiguratorFromQueryString();
+		builtQuery = configure(builtQuery, configuratorSkipSteps);
+		renderChart(printerEl, builtQuery, chart);
+	} catch (err) {
+		displayError(printerEl, err, null, chart);
+	}
+}
+
 export function init () {
-
-	const configure = getConfigurator();
-
 	[].forEach.call(document.querySelectorAll('.chart'), el => {
 		const chartName = el.dataset.chartName;
 		const printerEl = el.querySelector('.chart__printer')
-		let conf;
-		if (conf = window.charts.find(c => c.name === chartName)) {
-			try {
-				let builtQuery = KeenQuery.buildFromAlias(conf);
-
-				// todo: default to column chart if it has dimension but no interval... or something
-				builtQuery = builtQuery.setPrinter(conf.printer || 'LineChart').tidy();
-
-				if (conf.hasConfigurableInterval) {
-					// avoid showing as big number when the default view could easily be converted to a line graph over time
-					if (builtQuery.dimension < 2 && (['AreaChart','LineChart','ColumnChart'].indexOf(conf.printer) > -1 || !conf.printer)) {
-						builtQuery = builtQuery.interval('d')
-					}
-				}
-
-				storeKq(chartName, builtQuery);
-
-				const configuratorSkipSteps = [];
-
-				if (!conf.hasConfigurableInterval) {
-					configuratorSkipSteps.push('interval');
-				}
-
-				if (!conf.hasConfigurableTimeframe) {
-					configuratorSkipSteps.push('timeframe');
-				}
-
-				builtQuery = configure(builtQuery, configuratorSkipSteps);
-				renderChart(printerEl, builtQuery, conf);
-			} catch (err) {
-				displayError(printerEl, err, null, conf);
-			}
+		let chart;
+		if (chart = window.charts.find(c => c.name === chartName)) {
+			buildAndRenderChart(chart, printerEl)
 		} else {
 			displayError(printerEl, `Invalid chart name: ${chartName}`);
 		}
