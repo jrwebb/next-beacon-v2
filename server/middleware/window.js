df45c5548f2ba04570f04bf472e71b1101beb874
Adam Braimbridge <adam.braimbridge@ft.com>
2016-06-03 10:35:33 +0100

    Update window.js

--- server/middleware/window.js
+++ server/middleware/window.js
@@ -2,7 +2,7 @@
 module.exports = function (req, res, next) {
 	res.locals.KEEN_PROJECT_ID = process.env.KEEN_PROJECT_ID;
 	res.locals.KEEN_READ_KEY = process.env.KEEN_READ_KEY;
-	res.locals.KEEN_MASTER_KEY = process.env.KEEN_MASTER_KEY;
+	res.locals.KEEN_MASTER = process.env.KEEN_MASTER;
 	res.locals.KEEN_HOST = 'https://keen-proxy.ft.com/3.0';
 	// default
 	res.locals.charts = [];
